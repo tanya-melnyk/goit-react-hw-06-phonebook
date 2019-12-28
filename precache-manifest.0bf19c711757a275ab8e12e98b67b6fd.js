877c965664a9c1f8fbad31b75c6cfc3acaf36c83
Tetiana Melnyk <prostotayna@gmail.com>
2019-12-28 13:56:21 +0200

    Updates

---
+++ precache-manifest.0bf19c711757a275ab8e12e98b67b6fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1642f4155c356108836124fdbfca56",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "c00614e3ba46c57307f2",
+    "url": "/goit-react-hw-06-phonebook/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "c00614e3ba46c57307f2",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.78b219a3.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.78b219a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "536627c6dddc0be2f84c",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.32ac9aa0.chunk.js"
+  },
+  {
+    "revision": "6484aee76b7c53ad04fa",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.ee85b948.js"
+  }
+]);
